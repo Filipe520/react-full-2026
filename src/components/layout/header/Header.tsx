5fac62f01ec58a1866ef8ce7acb8d954a1ef50f5
Filipe Alves <filipealves520@gmail.com>
2026-07-30 22:25:46 -0300

    Atualizar o Header.tsx

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import Button from "../../Button";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-2 w-full max-w-[1200px] mx-auto fixed top-0 left-1/2 -translate-x-1/2 backdrop-blur-2xl">
+    <header className="flex justify-between items-center p-2 w-full max-w-[1200px] mx-auto fixed top-0 left-1/2 -translate-x-1/2 z-20 backdrop-blur-2xl">
       <img
         src="./logo.png"
         alt="logo website"
